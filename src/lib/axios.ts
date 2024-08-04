d68fa495e5efe5789789b496ac4f9f4e693f9a69
khalifan-kfan <khalifanmuwonge@gmail.com>
2024-08-04 21:36:24 +0300

    chore:try to fix axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { toast } from "sonner";
 
+axios.defaults.withCredentials = true
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true, 
