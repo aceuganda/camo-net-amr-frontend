980b01c0e595be607806318127b7f676f0fd8efd
khalifan-kfan <khalifanmuwonge@gmail.com>
2024-09-24 14:40:20 +0300

    chore: enable download of economic data

--- src/lib/hooks/useDataSets.ts
+++ src/lib/hooks/useDataSets.ts
@@ -1,17 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
-import api from './../axios';
-
-
+import api from "./../axios";
 
 export const downloadData = async (source: string) => {
-    const response = await api.get(`/data/download?source=${source}`,{
-      responseType: 'blob', 
-    });
-    return response.data; 
-  };
-  
-export const requestAccess = async (data:any) => {
-    const response = await api.post('/permissions/request', data);
-    return response.data; 
-  };
-  
+  let endpoint = `/data/download?source=${source}`;
+  if (source === "economic") {
+    endpoint = `/data/download/economic`;
+  }
+
+  const response = await api.get(endpoint, {
+    responseType: "blob",
+  });
+  return response.data;
+};
+
+export const requestAccess = async (data: any) => {
+  const response = await api.post("/permissions/request", data);
+  return response.data;
+};
