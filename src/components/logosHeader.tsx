509b9bb60e00a7f66b097a6693fa818bb4be2049
Khalifan Muwonge <khalifanmuwonge@gmail.com>
2024-10-08 15:38:09 +0300

    improve: home page (#1)

--- src/components/logosHeader.tsx
+++ src/components/logosHeader.tsx
@@ -1,17 +1,19 @@
 import HomeCatalogue from "@/components/landingCatalogue";
+import { link } from "fs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function LogoHeader() {
   const images = [
-    { src: "/logos/cnetug.webp", alt: "" },
-    { src: "/logos/idmak.webp", alt: " " },
-    { src: "/logos/ace.webp", alt: "ACE" },
-    { src: "/logos/welc.webp", alt: "" },
+    { src: "/logos/cnetug.webp", alt: "", link: "https://camonet.org" },
+    { src: "/logos/idmak.webp", alt: " ", link: "https://idi.mak.ac.ug"  },
+    { src: "/logos/ace.webp", alt: "ACE", link: "https://ace.ac.ug"  },
+    { src: "/logos/welc.webp", alt: "", link: "https://wellcome.org"  },
   ];
   return (
       <div className="bg-white w-full h-auto pb-[1rem] flex items-center justify-around">
         {images.map((image) => (
-          <div key={image.src} className="relative ">
+          <Link href={image.link} key={image.src} className="relative ">
             <Image
               src={image.src}
               alt={image.alt}
@@ -19,7 +21,7 @@ export default function LogoHeader() {
               width={100}
               height={70}
             />
-          </div>
+          </Link>
         ))}
       </div>
   );
