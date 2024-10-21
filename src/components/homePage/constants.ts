e388b9a7b999f63115a3e807d733881b8deb95cb
khalifan-kfan <khalifanmuwonge@gmail.com>
2024-10-21 18:11:31 +0300

    fix query filter

--- src/components/homePage/constants.ts
+++ src/components/homePage/constants.ts
@@ -1,5 +1,5 @@
 export const organisms = [
     { value: 'ecoli', name: 'E. coli' },
     { value: 'aureus', name: 'S. aureus' },
-    { value: 'pneumoniae', name: 'K. pneumoniae' },
+    { value: 'k.pneumoniae', name: 'K. pneumoniae' },
 ];
