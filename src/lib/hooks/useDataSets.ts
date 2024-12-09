57c70e7fc68d7fed40eedb21becb2ad74cf62e30
khalifan-kfan <khalifanmuwonge@gmail.com>
2024-12-09 19:02:53 +0300

    feat: enable variable selection

--- src/lib/hooks/useDataSets.ts
+++ src/lib/hooks/useDataSets.ts
@@ -1,17 +1,34 @@
 import { useQuery } from "@tanstack/react-query";
 import api from "./../axios";
 
-export const downloadData = async (source: string) => {
+export const downloadData = async (source: string, selectedVariables: string[]) => {
   let endpoint = `/data/download?source=${source}`;
   if (source === "economic") {
     endpoint = `/data/download/economic`;
   }
 
-  const response = await api.get(endpoint, {
-    responseType: "blob",
+  const response = await api.post(endpoint, {
+    selected_variables: selectedVariables,
+  }, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    responseType: "blob", 
   });
+
   return response.data;
 };
+
+export const useDatasetVariables = (source:string) => {
+  return useQuery<any, Error, {data: any}>({
+    queryFn: () => api.get(`/data/amr/dictionary?source=${source}`),
+    queryKey: ["amr_dictionary", source],
+    meta: {
+      errorMessage: "Failed to fetch dictionary"
+    }
+  });
+}
+
 
 export const requestAccess = async (data: any) => {
   const response = await api.post("/permissions/request", data);
