c5637cc4fd6581b1ed46864db8a85be10eebc552
khalifan-kfan <khalifanmuwonge@gmail.com>
2025-03-31 16:37:35 +0300

    remove data over flow

--- src/components/dataDetails/index.tsx
+++ src/components/dataDetails/index.tsx
@@ -748,8 +748,9 @@ export default function DatasetDetails({ id }: any) {
                               className="border rounded-lg p-4 bg-gray-50 shadow-sm hover:shadow-md transition-shadow"
                             >
                               <div className="flex justify-between items-center">
-                                <p className="text-sm font-medium text-gray-900 truncate">
-                                  {perm.project_title || "Untitled Project"}
+                                <p className="text-sm max-w-[7rem] font-medium text-gray-900 truncate">
+                                  {/* {perm.project_title || "Untitled Project"} */}
+                                  bvadhlbfjgaidfhriuehaouwehusdifiefiuwehuf
                                 </p>
                                 <span className={getStatusBadge(perm.status)}>
                                   {perm.status}
